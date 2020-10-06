c737b0db05a018585ec5e93a2bb114c22cc8b67d
Jane Austen <72279689+jane-austen@users.noreply.github.com>
2020-10-05 18:48:25 -0600

    Added jane method (#1)

--- Sample.java
+++ Sample.java
@@ -2,5 +2,10 @@ public class Sample {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("Hello World!");
+		jane();
+	}
+
+	public static void jane() {
+		System.out.println("Jane!");
 	}
 }
