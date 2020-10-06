8369e3eae330ad80e90b6f6629ba442c48a7e1ed
mary-shelley-seng696 <mary.shelley.seng696@protonmail.com>
2020-10-06 00:25:47 +0000

    Revert "Added mary method"
    This reverts commit 986eeab6049ffd98df71c223d4189c3e5f2f4241.

--- Sample.java
+++ Sample.java
@@ -2,10 +2,5 @@ public class Sample {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("Hello World!");
-		mary();
-	}
-
-	private static void mary() {
-		System.out.println("Mary!");
 	}
 }
