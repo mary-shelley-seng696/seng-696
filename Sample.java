986eeab6049ffd98df71c223d4189c3e5f2f4241
mary-shelley-seng696 <mary.shelley.seng696@protonmail.com>
2020-10-06 00:24:25 +0000

    Added mary method

--- Sample.java
+++ Sample.java
@@ -2,5 +2,10 @@ public class Sample {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("Hello World!");
+		mary();
+	}
+
+	private static void mary() {
+		System.out.println("Mary!");
 	}
 }
